576d4aade2c8f9a4dbd31a28a72ce18509f4a40d
Fox8081 <mendhulekartik@gmail.com>
2025-10-07 22:18:56 +0530

    fix: Update PYTHONPATH in run script

--- run.sh
+++ run.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-# Set the cache directory to a writable location inside our app folder
+# Set the cache directory to a writable location
 export TRANSFORMERS_CACHE='/app/cache'
+
+# Add the app directory to Python's path so it can find the 'backend' module
+export PYTHONPATH=/app
 
 # Run the gunicorn server
 gunicorn --bind 0.0.0.0:7860 backend.main:app
